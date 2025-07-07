d6203789bd6a2a0fbb190bba160bbf86b1c2bcb1
Sander <sander.jonkers+github@gmail.com>
2025-07-07 08:33:04 +0200

    Update SIMD_checker.sh

--- SIMD_checker.sh
+++ SIMD_checker.sh
@@ -15,14 +15,14 @@ echo -n $1 " "
 ARCH_INFO=$(file "$BINARY")
 #echo -n "$ARCH_INFO"
 
-if echo -n "$ARCH_INFO" | grep -q "ELF"; then
+if echo "$ARCH_INFO" | grep -q "ELF"; then
     echo -n "ELF binary. "
 else
         echo "No ELF. Exiting."
         exit 1
 fi
 
-if echo -n "$ARCH_INFO" | grep -q "x86-64"; then
+if echo "$ARCH_INFO" | grep -q "x86-64"; then
     echo -n "Detected x86_64 "
     #./run_x86.sh "$BINARY"
     if objdump -d $1 | grep -q -E 'xmm|ymm|zmm'; then
@@ -33,7 +33,7 @@ if echo -n "$ARCH_INFO" | grep -q "x86-64"; then
         exit 1
     fi
 
-elif echo -n "$ARCH_INFO" | grep -qi "AArch64"; then
+elif echo "$ARCH_INFO" | grep -qi "AArch64"; then
     echo -n "Detected ARM64 (AArch64) "
     #./run_arm.sh "$BINARY"
     if objdump -d $1 | grep -q -E 'v[0-9]+|q[0-9]+'; then
@@ -44,7 +44,7 @@ elif echo -n "$ARCH_INFO" | grep -qi "AArch64"; then
         exit 1
     fi
 
-elif echo -n "$ARCH_INFO" | grep -qi "RISC-V "; then
+elif echo "$ARCH_INFO" | grep -qi "RISC-V "; then
     echo -n "Detected RISC-V "
     #./run_riscv.sh "$BINARY"
     if objdump -d $1 | grep -q -E 'v[0-9]+'; then
@@ -58,12 +58,12 @@ elif echo -n "$ARCH_INFO" | grep -qi "RISC-V "; then
 # Older ISA's
 
 # are these still around?
-elif echo -n "$ARCH_INFO" | grep -q "Intel 80386"; then
+elif echo "$ARCH_INFO" | grep -q "Intel 80386"; then
     echo -n "Detected x86 (32-bit) "
     ./run_x86.sh "$BINARY"
 
 # check on old Raspi?
-elif echo -n "$ARCH_INFO" | grep -qi "ARM"; then
+elif echo "$ARCH_INFO" | grep -qi "ARM"; then
     echo -n "Detected ARM "
     ./run_arm.sh "$BINARY"
 
